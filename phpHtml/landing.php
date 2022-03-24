0207c91e2c07fed155e6d5df855acc0a5023ea33
Nathan COLLINET <nathan.collinet@etu.univ-nantes.fr>
2022-03-24 10:33:39 +0000

    Upload New File

---
+++ phpHtml/landing.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="../CSS/landing.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <div id="container">
+            <label><h1>A la recherche d'un livre ?</h1></label>
+        </div>
+        <div id="h2">
+            <label><h2>Vous êtes au bon endroit !</h2></label>
+        </div>
+        <div id="bouton">
+            <a href="login.php"><input type="submit" name="continuer" id="1" value="Se connecter"></a>
+        </div>
+    </body>
