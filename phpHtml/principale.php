5a4bf21c8ae27b4290f3a8c67aad7e37a196325e
Nathan COLLINET <nathan.collinet@etu.univ-nantes.fr>
2022-03-24 11:21:29 +0000

    Replace principale.php

--- phpHtml/principale.php
+++ phpHtml/principale.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
@@ -12,10 +13,23 @@
             <!-- tester si l'utilisateur est connecté -->
             <?php
                 session_start();
+
+                if($_SESSION['username'] == ""){
+                    header("location:login.php");
+                }
+
                 if(isset($_GET['deconnexion']))
                 { 
                    if($_GET['deconnexion']==true)
                    {  
+                    $_SESSION = array();
+                    if (ini_get("session.use_cookies")) {
+                        $params = session_get_cookie_params();
+                        setcookie(session_name(), '', time() - 42000,
+                            $params["path"], $params["domain"],
+                            $params["secure"], $params["httponly"]
+                        );
+                    }
                       session_destroy();
                       header("location:login.php");
                    }
