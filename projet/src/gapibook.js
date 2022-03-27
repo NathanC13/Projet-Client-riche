77c5e6f8d3da49c7b50985524739233d721589db
Florian CHACUN <florian.chacun@etu.univ-nantes.fr>
2022-03-27 01:07:38 +0000

    Update gapibook.js

--- projet/src/gapibook.js
+++ projet/src/gapibook.js
@@ -1,5 +1,3 @@
-//const API_KEY = 'f45f7a43';
-
 const useGoogleBookApi = {
   bySearch: (search) =>
     new Promise((resolve, reject) => {
@@ -14,6 +12,26 @@ const useGoogleBookApi = {
   byId: (search) =>
     new Promise((resolve, reject) => {
       const BOOK_API_URL = `https://www.googleapis.com/books/v1/volumes/${search}`;
+
+      fetch(BOOK_API_URL)
+        .then((response) => response.json())
+        .then((jsonResponse) => resolve(jsonResponse))
+        .catch((err) => reject(err));
+    }),
+
+  byEbook: (search) =>
+    new Promise((resolve, reject) => {
+      const BOOK_API_URL = `https://www.googleapis.com/books/v1/volumes?q=${search}&filter=ebooks`;
+
+      fetch(BOOK_API_URL)
+        .then((response) => response.json())
+        .then((jsonResponse) => resolve(jsonResponse))
+        .catch((err) => reject(err));
+    }),
+
+  byEbookFree: (search) =>
+    new Promise((resolve, reject) => {
+      const BOOK_API_URL = `https://www.googleapis.com/books/v1/volumes?q=${search}&filter=free-ebooks`;
 
       fetch(BOOK_API_URL)
         .then((response) => response.json())
