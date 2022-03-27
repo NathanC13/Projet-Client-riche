df43348a41d557dc69c79fd51ec724af2c1f3ade
Florian CHACUN <florian.chacun@etu.univ-nantes.fr>
2022-03-27 01:08:17 +0000

    Update book.js

--- projet/src/components/book.js
+++ projet/src/components/book.js
@@ -3,25 +3,22 @@ import gapibook from "../gapibook.js";
 Vue.component("book", {
   props: ["book"],
   template: `
-    <div class="col s12 m6">
+    <div class="col s6 m3">
       <div class="card large">
             <div class="card-image">
 
-              <img :src="book.volumeInfo.imageLinks.thumbnail=== undefined? 'assets/img/defaut.jpg' : book.volumeInfo.imageLinks.thumbnail"> 
+              <img :src="book.volumeInfo.imageLinks === undefined? 'assets/img/defaut.jpg' : book.volumeInfo.imageLinks.thumbnail"> 
 
 
-              <!--    <img src="assets/img/defaut.jpg" v-if="book.volumeInfo.imageLinks.thumbnail=== undefined"  >
-            <img  src=" book.volumeInfo.imageLinks.thumbnail" v-else> -->
-
-
-            <span class="card-title"> {{book.volumeInfo.title}} </span>
+            
             </div>
        
             <div class="card-content">
-                <p> {{book.volumeInfo.publishedDate}} </p>
+            <span class="card-title"> {{book.volumeInfo.title}} </span>
+                <p> {{book.volumeInfo.publishedDate.substring(0,4)}} </p>
             </div>
             <div class="card-action">
-                <a href="#app" @click="handleSelected">show more</a>
+                <a href="#app" @click="handleSelected">En savoir plus</a>
         </div>
       </div>
     </div>`,
