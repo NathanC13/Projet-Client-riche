7b9dbf3df4f8be83c19fe00628edbafb8c809ac7
Florian CHACUN <florian.chacun@etu.univ-nantes.fr>
2022-03-27 01:08:02 +0000

    Update app.js

--- projet/src/components/app.js
+++ projet/src/components/app.js
@@ -1,14 +1,27 @@
 Vue.component("app", {
   template: `<div  id="app"> 
+
+                  
+    <p> Consulter nos livres ! </p>
+    <label>
+                        <input  name="group1" id="eBook" type="checkbox"/>
+                        <span>eBook</span>
+                        </label>
+
+                        <label>
+                        <input name="group1" id="free" type="checkbox"/>
+                        <span>Gratuit</span>
+                        </label>
+        
                     <search  @search-done="searchCompleted($event)"> </search>
-                    <div class="col s8">
+                    <div class="col s6">
                         <div class="row">
                             <book v-for="book in books" v-bind:key="book.id" :book=book v-on:book-selected="bookSelected($event)"> </book>
                         </div>
                     </div>
-                    <div class="col s4">
+                    <div class="col s6">
                         <div class="row">
-                            <detail v-bind:bookId="selectedId" :key="selectedId"> </detail>
+                            <detail  v-bind:bookId="selectedId" :key="selectedId"> </detail>
                         </div>
                     </div>
                </div>`,
@@ -20,9 +33,27 @@ Vue.component("app", {
   },
   methods: {
     searchCompleted: function (data) {
+      // Si le json n'a pas été récuperer ou qu'il n'a pas été définie (absence de chaine dans le champ)
       if (data != undefined) {
+        // On vérifie si le bouton  "eBook" est coché
+        /*  if (
+          document.getElementById("eBook").checked &&
+          document.getElementById("free").checked == false
+        ) {
+          eBook = []; // si c'est le cas on crée un tableau qui servira de données a afficher
+          data.forEach((element) => {
+            if (element.saleInfo.isEbook == true) {
+              // pour chaque élément de l'item, on verifie si l'attribut eBook est vrai
+              eBook.push(element); // on push si c'est le cas
+            }
+
+            this.books = eBook; // les livres a afficher sont données au tableau
+            this.selectedId = null;
+          });
+        } else {*/
         this.books = data;
         this.selectedId = null;
+        // }
       }
     },
     bookSelected: function (id) {
