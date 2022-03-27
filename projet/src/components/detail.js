5396eaa74e2dc0e50cf3c8242c80f03585b6f0e6
Florian CHACUN <florian.chacun@etu.univ-nantes.fr>
2022-03-27 01:08:36 +0000

    Update detail.js

--- projet/src/components/detail.js
+++ projet/src/components/detail.js
@@ -1,35 +1,32 @@
 import useGoogleBookApi from "../gapibook.js";
 
 Vue.component("detail", {
-  props: ["bookId"],
+  props: ["bookId", "show"],
   template: `
     <table class="striped">
        <tbody>
-            <tr> <td> Titre </td> <td> {{detail.volumeInfo.title}} </td></tr>
-            <tr> <td> Année </td> <td> {{detail.volumeInfo.publishedDate}} </td></tr>
-            <tr> <td> Nombre de pages </td> <td> {{detail.volumeInfo.pageCount}} </td></tr>
-            <tr> <td> Genre </td> <td> {{detail.Genre}} </td></tr>
-            <tr> <td> Auteur </td> <td> {{detail.volumeInfo.authors}} </td></tr>
-            <tr> <td> Description </td> <td> {{detail.volumeInfo.description}} </td></tr>
+            <tr> <td> Titre </td> <td> {{detail.volumeInfo.title === undefined? 'Titre non connu' :detail.volumeInfo.title}} </td></tr>
+            <tr> <td> Année </td> <td> {{detail.volumeInfo.publishedDate === undefined? 'Année non connu' :detail.volumeInfo.publishedDate.substr(0, 4)}} </td></tr>
+            <tr> <td> Pages </td> <td> {{detail.volumeInfo.pageCount === undefined? 'Nombre de pages non-connu' : detail.volumeInfo.pageCount}} </td></tr>
+            <tr> <td> Genre </td> <td> {{detail.volumeInfo.categories === undefined? 'Pas de catégorie' : detail.volumeInfo.categories[0]}} </td></tr>
+            <tr> <td> Auteur </td> <td> {{detail.volumeInfo.authors === undefined? 'Auteurs non connus' : detail.volumeInfo.authors[0]}} </td></tr>
+            <tr> <td> Description </td> <td> {{detail.volumeInfo.description === undefined? 'Description non connu' : detail.volumeInfo.description}} </td></tr>
       </tbody>
   </table>`,
   data: function () {
     return {
       detail: {
         title: null,
-        Year: null,
-        Released: null,
-        pageCount: null,
-        Genre: null,
-        Author: null,
-        Writer: null,
-        Actors: null,
-        Descritpion: null,
-        Language: null,
-        Country: null,
-        Awards: null,
-        Poster: null,
-        Ratings: [],
+        subtitle: null,
+        authors: [],
+        publisher: null,
+        publishedDate: null,
+        industryIdentifiers: [],
+        language: null,
+        country: null,
+        amount: null,
+        buyLink: null,
+        isEbook: null,
       },
     };
   },
